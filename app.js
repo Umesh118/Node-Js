779635065d6ccdefdbaf1503713ff33c5b5b7f7c
Umesh <umeshiitbombay@gmail.com>
2021-12-05 20:52:51 +0530

    Express Generator Rest API

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const dishesRouter = require('./routes/dishesRouter');
+const leaderRouter = require('./routes/leadersRouter');
+const promotionRouter = require('./routes/promotionsRouter');
 
 var app = express();
 
@@ -21,14 +24,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishesRouter);
+app.use('/promotions', promotionRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
