756c0542c94f95e772bcca005b9cb479e42212d3
Umesh <umeshiitbombay@gmail.com>
2021-12-05 20:01:21 +0530

    Express Router

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express'),
     http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const dishesRouter = require('./routes/dishesRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,15 +11,10 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
-});
+app.use('/dishes', dishesRouter);
 
 const server = http.createServer(app);
 
